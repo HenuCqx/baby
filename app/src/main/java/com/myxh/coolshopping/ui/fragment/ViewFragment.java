0a08bd39044d93b1d35b676671185fbc38069cd3
chenqingxiang <1510120023@vip.henu.edu.cn>
2018-06-05 15:01:39 +0800

    新增功能

--- app/src/main/java/com/myxh/coolshopping/ui/fragment/ViewFragment.java
+++ app/src/main/java/com/myxh/coolshopping/ui/fragment/ViewFragment.java
@@ -1,15 +1,21 @@
 package com.myxh.coolshopping.ui.fragment;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.annotation.Nullable;
 import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 import android.widget.VideoView;
 
 import com.myxh.coolshopping.R;
@@ -17,23 +23,29 @@ import com.myxh.coolshopping.network.HttpListener;
 import com.myxh.coolshopping.ui.base.BaseFragment;
 import com.yolanda.nohttp.rest.Response;
 
-public class ViewFragment extends BaseFragment implements HttpListener<String> {
+public class ViewFragment extends BaseFragment implements HttpListener<String> ,View.OnTouchListener {
 
+    private GestureDetector mGestureDetector;
     private VideoView videoView;
+    private static final int FLING_MIN_DISTANCE = 50;   //最小距离
+    private static final int FLING_MIN_VELOCITY = 0;  //最小速度
+    static int page = 1;
+    WebView webView;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_look, null);
         initPopMenu();
-        WebView webView = (WebView)view.findViewById(R.id.webgif);
-
-        WebSettings webSettings = webView.getSettings();
-
+        webView = (WebView)view.findViewById(R.id.webgif);
         WebSettings settings = webView.getSettings();
         settings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
 
         webView.setInitialScale(100);
-        webView.loadUrl("file:///android_asset/gif.html");
+        webView.loadUrl("file:///android_asset/gif_1.html");
+        mGestureDetector = new GestureDetector(getActivity(), myGestureListener);
+        WebView mLl = (WebView) view.findViewById(R.id.webgif);//布局的主容器
+        mLl.setOnTouchListener(this);//将主容器的监听交给本activity，本activity再交给mGestureDetector
+        mLl.setLongClickable(true);   //必需设置这为true 否则也监听不到手势
         return view;
     }
 
@@ -55,6 +67,47 @@ public class ViewFragment extends BaseFragment implements HttpListener<String> {
 
     }
 
+    GestureDetector.SimpleOnGestureListener myGestureListener = new GestureDetector.SimpleOnGestureListener(){
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
 
+            Log.e("<--滑动测试-->", "开始滑动");
+            float x = e1.getX()-e2.getX();
+            float x2 = e2.getX()-e1.getX();
+            if(x>FLING_MIN_DISTANCE&&Math.abs(velocityX)>FLING_MIN_VELOCITY){
+                //Toast.makeText(getActivity(), "向左手势", Toast.LENGTH_SHORT).show();
+                if(page < 3){
+                    page +=1;
+                }else if (page == 3){
+                    page =1;
+                }
+                getPage();
 
+            }else if(x2>FLING_MIN_DISTANCE&&Math.abs(velocityX)>FLING_MIN_VELOCITY){
+                //Toast.makeText(getActivity(), "向右手势", Toast.LENGTH_SHORT).show();
+                if(page > 1){
+                    page -=1;
+                }else if (page == 1){
+                    page =3;
+                }
+                getPage();
+            }
+
+            return false;
+        }
+    };
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        return  mGestureDetector.onTouchEvent(motionEvent);
+    }
+
+    void getPage(){
+        if(page == 1){
+            webView.loadUrl("file:///android_asset/gif_1.html");
+        }else if(page == 2){
+            webView.loadUrl("file:///android_asset/gif_2.html");
+        }else if(page == 3){
+            webView.loadUrl("file:///android_asset/gif_3.html");
+        }
+    }
 }
